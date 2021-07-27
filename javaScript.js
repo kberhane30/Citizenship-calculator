3cc7cb25eaa00a8118960887306c16282665370b
Khaled <65722140+kberhane30@users.noreply.github.com>
2021-07-26 21:25:08 -0400

    Add files via upload
    The calculator now can do the following: 
        1- Get the date the user enters OR choose from the date input
        2- Add five years to the chosen entry. 
        3- Display how much time the user has in the U.S via the Console.log();

---
+++ javaScript.js
@@ -0,0 +1,46 @@
+
+
+let selectDate = document.getElementById('Calender');
+let button = document.getElementById('result');
+let ShowResult = document.getElementById('Result');
+let popup = document.querySelector('.popup');
+
+window.onload = popup.classList.remove('popup');
+
+
+button.addEventListener('click',()=>{
+
+     // let m = moment(selectDate.value);
+     // let applyingDate = m.add(5,"years");
+     // let cur = moment();
+     // let diff = applyingDate - cur;
+     
+     // let years = Math.floor(diff/(1000*60*60*24)/365);
+
+     // console.log(applyingDate.format('MMMM/DD/YYYY'));
+
+
+     
+
+     const x= moment();
+     const y = moment(selectDate.value);
+     const period = moment.duration(x.diff(y));
+
+console.log(period.get('years'));
+console.log(period.get('month'));
+console.log(period.get('days'));
+
+
+     
+      
+      
+     
+
+
+// ShowResult.innerText = `Your best day to apply will be on: ${applyingDate.toString()}`;
+//     ShowResult.innerText = `The time left until you apply to your citizenship is : ${years} years and ${months} months and ${days} days so make sure you are prepared, Good luck`;
+})
+
+
+
+
