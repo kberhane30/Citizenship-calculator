4bd470eed4613296815ef8ffdaa7bde0ea7310b5
Khaled <khaledberhane91@hotmail.com>
2021-07-27 10:17:56 -0400

    Result cell added
    Added two cells to display the result, one for the how much time the user has been in the U.S and the second cell to inform the user when he can apply for the Citizenship

--- javaScript.js
+++ javaScript.js
@@ -4,23 +4,21 @@ let selectDate = document.getElementById('Calender');
 let button = document.getElementById('result');
 let ShowResult = document.getElementById('Result');
 let popup = document.querySelector('.popup');
-
+let dur = document.querySelector('.Dur');
+let apply = document.querySelector('.Apply');
 window.onload = popup.classList.remove('popup');
 
 
 button.addEventListener('click',()=>{
 
-     // let m = moment(selectDate.value);
-     // let applyingDate = m.add(5,"years");
-     // let cur = moment();
-     // let diff = applyingDate - cur;
+     let m = moment(selectDate.value);
+     let applyingDate = m.add(5,"years");
+     let cur = moment();
+     let diff = applyingDate - cur;
      
      // let years = Math.floor(diff/(1000*60*60*24)/365);
 
      // console.log(applyingDate.format('MMMM/DD/YYYY'));
-
-
-     
 
      const x= moment();
      const y = moment(selectDate.value);
@@ -30,11 +28,12 @@ console.log(period.get('years'));
 console.log(period.get('month'));
 console.log(period.get('days'));
 
+console.log(applyingDate.format('MMM/DD/YYYY'));
 
-     
-      
-      
-     
+dur.innerText = period.get('years') +" years " + period.get('month') + " months " + period.get('days') + " Days ";
+apply.innerText = applyingDate.format('MMM/DD/YYYY');
+
+
 
 
 // ShowResult.innerText = `Your best day to apply will be on: ${applyingDate.toString()}`;
